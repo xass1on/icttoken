010b7ca286dd8fa3454e2398cf4f127e46b22938
Khashkhuu Gankhuyag <xass1on@gmail.com>
2021-05-18 15:05:27 +0800

    update

--- contracts/ICTToken.sol
+++ contracts/ICTToken.sol
@@ -11,20 +11,22 @@ contract ICTToken is ERC777, Ownable {
 
     uint256 constant _totalSupply = 1428600000;
 
-    uint256 constant _tokenReserve = 180660460;
     uint256 constant _tokenToOffer = 124793954;
-    uint256 constant _tokenToDistribute1 = 1123145586;
-    uint256 constant _tokenToDistribute2 = 1123145586;
-    uint256 constant _tokenToDistribute3 = 1123145586;
-    uint256 constant _tokenToDistribute4 = 1123145586;
+    uint256 constant _tokenReserve1 = 90330230;
+    uint256 constant _tokenReserve2 = 90330230;
+    uint256 constant _tokenToDistribute1 = 280786396;
+    uint256 constant _tokenToDistribute2 = 280786396;
+    uint256 constant _tokenToDistribute3 = 280786397;
+    uint256 constant _tokenToDistribute4 = 280786397;
 
     string constant _tokenName = "ICTToken";
     string constant _tokenSymbol = "ICTT";
 
-    address constant _mainAddress = 0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
-    address constant _tokenReserveAddress =
-        0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
     address constant _tokenOfferAddress =
+        0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
+    address constant _tokenReserveAddress1 =
+        0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
+    address constant _tokenReserveAddress2 =
         0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
     address constant _tokenDistributeAddres1 =
         0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
@@ -36,9 +38,9 @@ contract ICTToken is ERC777, Ownable {
         0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
 
     constructor() ERC777(_tokenName, _tokenSymbol, new address[](0)) {
-        // _mint(msg.sender, initialSupply * 10**18, "", "");
         _mint(_tokenOfferAddress, _tokenToOffer * 10**18, "", "");
-        _mint(_tokenReserveAddress, _tokenReserve * 10**18, "", "");
+        _mint(_tokenReserveAddress1, _tokenReserve1 * 10**18, "", "");
+        _mint(_tokenReserveAddress2, _tokenReserve2 * 10**18, "", "");
         _mint(_tokenDistributeAddres1, _tokenToDistribute1 * 10**18, "", "");
         _mint(_tokenDistributeAddres2, _tokenToDistribute2 * 10**18, "", "");
         _mint(_tokenDistributeAddres3, _tokenToDistribute3 * 10**18, "", "");
