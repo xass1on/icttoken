1751e65fcccb3185e744eea87a053890f5c76c40
Khashkhuu Gankhuyag <xass1on@gmail.com>
2021-05-18 13:57:31 +0800

    update

--- contracts/ICTToken.sol
+++ contracts/ICTToken.sol
@@ -9,12 +9,40 @@ import "@openzeppelin/contracts/utils/math/Math.sol";
 contract ICTToken is ERC777, Ownable {
     using Math for uint256;
 
-    constructor(
-        string memory name,
-        string memory symbol,
-        uint256 initialSupply
-    ) ERC777(name, symbol, new address[](0)) {
-        _mint(msg.sender, initialSupply * 10**18, "", "");
+    uint256 constant _totalSupply = 1428600000;
+
+    uint256 constant _tokenReserve = 180660460;
+    uint256 constant _tokenToOffer = 124793954;
+    uint256 constant _tokenToDistribute1 = 1123145586;
+    uint256 constant _tokenToDistribute2 = 1123145586;
+    uint256 constant _tokenToDistribute3 = 1123145586;
+    uint256 constant _tokenToDistribute4 = 1123145586;
+
+    string constant _tokenName = "ICTToken";
+    string constant _tokenSymbol = "ICTT";
+
+    address constant _mainAddress = 0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
+    address constant _tokenReserveAddress =
+        0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
+    address constant _tokenOfferAddress =
+        0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
+    address constant _tokenDistributeAddres1 =
+        0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
+    address constant _tokenDistributeAddres2 =
+        0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
+    address constant _tokenDistributeAddres3 =
+        0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
+    address constant _tokenDistributeAddres4 =
+        0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
+
+    constructor() ERC777(_tokenName, _tokenSymbol, new address[](0)) {
+        // _mint(msg.sender, initialSupply * 10**18, "", "");
+        _mint(_tokenDistributeAddres1, _tokenToDistribute1 * 10**18, "", "");
+        _mint(_tokenDistributeAddres2, _tokenToDistribute2 * 10**18, "", "");
+        _mint(_tokenDistributeAddres3, _tokenToDistribute3 * 10**18, "", "");
+        _mint(_tokenDistributeAddres4, _tokenToDistribute4 * 10**18, "", "");
+        _mint(_tokenOfferAddress, _tokenToOffer * 10**18, "", "");
+        _mint(_tokenReserveAddress, _tokenReserve * 10**18, "", "");
         transferOwnership(msg.sender);
     }
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -10,18 +10,5 @@ module.exports = async function (deployer, network, accounts) {
     // In a test environment an ERC777 token requires deploying an ERC1820 registry
     await singletons.ERC1820Registry(accounts[0])
   }
-
-  /***
-   * ICTToken 
-   */
-  const totalSupply = new BN("1 428 600 000")
-  const tokenReserve = new BN("180 660 460")
-  const tokenToOffer = new BN("124 793 954")
-  const tokenToDistribute = new BN("1 123 145 586")
-  const tokenName = "ICTToken"
-  const tokenSymbol = "ICTT"
-  const tokenReserveAddress = "0xc5af4aA1359633F71d8c6Cb7404D46c8415C8D86"
-
-  await deployer.deploy(ICTToken, tokenName, tokenSymbol, totalSupply)
-  const _token = await ICTToken.deployed()
+  await deployer.deploy()
 }
