3e7f3266a26f546da2c7f31b9cdd8e61a2bde513
Khashkhuu Gankhuyag <xass1on@gmail.com>
2021-05-20 10:58:18 +0800

    update

--- contracts/ICTToken.sol
+++ contracts/ICTToken.sol
@@ -20,22 +20,22 @@ contract ICTToken is ERC777, Ownable {
     uint256 constant _tokenToDistribute4 = 280786397;
 
     string constant _tokenName = "ICTToken";
-    string constant _tokenSymbol = "ICTT";
+    string constant _tokenSymbol = "ICTG";
 
     address constant _tokenOfferAddress =
-        0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
+        0x19Dba30a1979813c7dFC099f85d180f2145C4318;
     address constant _tokenReserveAddress1 =
-        0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
+        0x70d52a41400b01427903b6a18AeD15b7Eb9AdA67;
     address constant _tokenReserveAddress2 =
-        0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
+        0x573C73Eaff7D111F964D91DDACFC2b69c85180d6;
     address constant _tokenDistributeAddres1 =
-        0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
+        0xcE0a2C3f51C9F3a68B67307f57C9b86dfe100E75;
     address constant _tokenDistributeAddres2 =
-        0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
+        0xc2cc60DD7eea6382Fdf74F3812bd73716b24225A;
     address constant _tokenDistributeAddres3 =
-        0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
+        0x0cf0b3c83AC0A78ceFD04d179D19B9537EbD5e00;
     address constant _tokenDistributeAddres4 =
-        0xCfc3dfD6e040A1C783eE4910916332Eb926D908A;
+        0x3f11Ca630d0d80BDC1b41F414958FFAa4D95Bc33;
 
     constructor() ERC777(_tokenName, _tokenSymbol, new address[](0)) {
         _mint(_tokenOfferAddress, _tokenToOffer * 10**18, "", "");
@@ -45,7 +45,6 @@ contract ICTToken is ERC777, Ownable {
         _mint(_tokenDistributeAddres2, _tokenToDistribute2 * 10**18, "", "");
         _mint(_tokenDistributeAddres3, _tokenToDistribute3 * 10**18, "", "");
         _mint(_tokenDistributeAddres4, _tokenToDistribute4 * 10**18, "", "");
-        transferOwnership(msg.sender);
     }
 
     fallback() external payable {
@@ -56,26 +55,11 @@ contract ICTToken is ERC777, Ownable {
         revert();
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        to;
-        amount;
+    function burn() public pure {
         revert();
     }
 
-    function burn(uint256 amount, bytes memory data) public override onlyOwner {
-        revert();
-    }
-
-    function operatorBurn(
-        address account,
-        uint256 amount,
-        bytes memory data,
-        bytes memory operatorData
-    ) public pure override {
-        account;
-        amount;
-        data;
-        operatorData;
+    function operatorBurn() public pure {
         revert();
     }
 }
